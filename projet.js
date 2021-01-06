f54b4b68d30b573151ad7bf3a9ce4304d5ecf429
BlasDams <samassaadama@hotmail.fr>
2021-01-06 16:36:37 +0100

    ajout de la fonction turnRight

--- projet.js
+++ projet.js
@@ -38,3 +38,27 @@ function turnLeft(rover){
     }
 };
 turnLeft(rover);
+
+function turnRight(rover){
+    switch(rover.direction){
+        case "N":
+            rover.direction = "E"
+            console.log(rover);
+        break;
+        case "E":
+            rover.direction = "S"
+            console.log(rover);
+            break;
+            case "S":
+            rover.direction = "W"
+            console.log(rover);
+        break;
+        case "W":
+            rover.direction = "N"
+            console.log(rover);
+            break;
+        default:
+            console.log("error");
+    }
+};
+turnRight(rover);
